22bcbe87fd7c723920dbb3dcc10a7cdffbea46de
Miguel Rugerio <mrugerio@gmail.com>
2023-08-03 16:10:12 -0600

    Inicio Streams

--- Streams/src/com/curso/v0/Principal2.java
+++ Streams/src/com/curso/v0/Principal2.java
@@ -2,9 +2,11 @@ package com.curso.v0;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Principal {
+public class Principal2 {
 	
 	private static Employee[] arrayOfEmps = { 
 			new Employee(0, "Patrobas", 100.0), 
@@ -18,34 +20,19 @@ public class Principal {
 		List<Employee> listEmpleados = Arrays.asList(arrayOfEmps);
 		
 		//1.Filtrar los que ganan menos de 400
-		
-		List<Employee> listaEmp2 = new ArrayList<>();
-		
-		for (Employee e:listEmpleados) {
-			if (e.getSalary()<400)
-				listaEmp2.add(e);
-		}
-		
-		//2.Incrementar su sueldo en 200
-		
-		for (Employee e: listaEmp2) {
-			e.setSalary(e.getSalary()+200);
-		}
-		
-		//3.Filtrar los que tengan una longitud mayor a 6 en su nombre
-		List<Employee> listaEmp3 = new ArrayList<>();
-		for (Employee e: listaEmp2) {
-			if (e.getNombre().length()>6)
-				listaEmp3.add(e);
-		}
-		
-		for (Employee e: listaEmp3) {
-			System.out.println(e);
-		}
-		
+		//2.Incrementar su sueldo en 200	
+		//3.Filtrar los que tengan una longitud mayor a 6 en su nombre	
 		//4.Ordernar por nombre
 		//5.Obtener una lista de los nombres: List<String> nombres
-
+		
+		List<String> listaNombres = listEmpleados.stream()
+				.filter(e->e.getSalary()<400)
+				.peek(y->y.setSalary(y.getSalary()+200))
+				.filter(z -> z.getNombre().length()>6)
+				.sorted(Comparator.comparing(Employee::getNombre))
+				.map(e -> e.getNombre()).collect(Collectors.toList());
+			
+		listaNombres.forEach(x -> System.out.println(x));
 		
 		
 	}
