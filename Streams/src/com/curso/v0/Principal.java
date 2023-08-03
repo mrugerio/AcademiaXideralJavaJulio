9b9c08b33d2829042942408b7881a724a4e1eb4a
Miguel Rugerio <mrugerio@gmail.com>
2023-08-03 15:20:49 -0600

    Inicio Streams

---
+++ Streams/src/com/curso/v0/Principal.java
@@ -0,0 +1,18 @@
+package com.curso.v0;
+
+public class Principal {
+	
+	private static Employee[] arrayOfEmps = { 
+			new Employee(0, "Patrobas", 100.0), 
+			new Employee(1, "Tercio", 200.0),
+			new Employee(2, "Andronico", 300.0), 
+			new Employee(3, "Epeneto", 400.0),
+			new Employee(4, "Filologo", 500.0) };
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
