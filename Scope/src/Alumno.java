ad77445a1fb7fd44c60708f9a547274bd504e721
Miguel Rugerio <mrugerio@gmail.com>
2023-08-16 10:08:51 -0600

    Demos Security con Jdbc & Demo Rest sin Exceptions

---
+++ Scope/src/Alumno.java
@@ -0,0 +1,16 @@
+
+public class Alumno {
+	
+	void getPromedio() {
+		
+		int x;
+		String name = null;
+		
+		x= 8;
+		
+		System.out.println(x);
+		System.out.println(name);
+
+	}
+
+}
---
+++ Scope/src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+	
+	
+	public static void main(String[] args) {
+		
+		//Variable Locales NO SE INICIALIZAN
+		String name = null; 
+		int contador = 0; 
+		
+		System.out.println(name);
+		System.out.println(contador);
+
+	}
+
+}
