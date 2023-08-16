236d6d90fa9773453f3005b734afd3f1b1ee0dfb
Miguel Rugerio <mrugerio@gmail.com>
2023-08-16 17:18:52 -0600

    Repaso Overloading

---
+++ Static/src/com/curso/v6/Pato.java
@@ -0,0 +1,18 @@
+package com.curso.v6;
+
+public class Pato {
+	
+	static String name = "Donald";
+	
+	static void nadar(String s) {
+		System.out.println(s);
+	}
+	
+	static void nadar(int i) {
+		System.out.println(i);
+	}
+	
+	static void nadar(String s, int i) {
+		System.out.println(s+i);
+	}
+}
---
+++ Static/src/com/curso/v6/Principal.java
@@ -0,0 +1,19 @@
+package com.curso.v6;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola");
+		
+		System.out.println(Pato.name);
+		
+		Pato.nadar("Nadar");
+		
+		Pato.nadar(7);
+		
+		Pato.nadar("Cadena",7);
+
+	}
+
+}
